c82d90a432ab6ab2527e849e024fc1b22109612b
Me The Admin <webcontentyo@gmail.com>
2020-05-05 17:57:24 +0200

    initial scene commit

---
+++ src/components/scene/FaceLogo.js
@@ -0,0 +1,40 @@
+import React, { Fragment, Suspense, useRef, useState, useRender } from 'react';
+import { Canvas, useThree, useFrame } from 'react-three-fiber';
+
+// import Scene from './Scene';
+import FaceV1 from './Facev1';
+
+const FaceLogo = () => {
+  const isBrowser = typeof window !== 'undefined';
+  const d = 8.25;
+  return (
+    <>
+      {isBrowser && (
+        <Canvas shadowMap pixelRatio={window.devicePixelRatio}>
+          <hemisphereLight
+            skyColor={'black'}
+            groundColor={0xffffff}
+            intensity={0.68}
+            position={[0, 50, 0]}
+          />
+          <directionalLight
+            position={[-8, 12, 8]}
+            shadow-camera-left={d * -1}
+            shadow-camera-bottom={d * -1}
+            shadow-camera-right={d}
+            shadow-camera-top={d}
+            shadow-camera-near={0.1}
+            shadow-camera-far={1500}
+            castShadow
+          />
+          <Suspense fallback={null}>
+            {/* <Scene /> */}
+            <FaceV1 />
+          </Suspense>
+        </Canvas>
+      )}
+    </>
+  );
+};
+
+export default FaceLogo;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,36 @@
-import React from "react"
+import React, { Fragment, Suspense } from 'react';
+import { Canvas } from 'react-three-fiber';
+import FaceV1 from './Facev1';
 
-export default () => <div>Hello world!</div>
+import '../App.css';
+
+const IndexPage = () => {
+  const d = 8.25;
+  return (
+    <Fragment>
+      <Canvas shadowMap pixelRatio={window.devicePixelRatio}>
+        <hemisphereLight
+          skyColor={'black'}
+          groundColor={0xffffff}
+          intensity={0.68}
+          position={[0, 50, 0]}
+        />
+        <directionalLight
+          position={[-8, 12, 8]}
+          shadow-camera-left={d * -1}
+          shadow-camera-bottom={d * -1}
+          shadow-camera-right={d}
+          shadow-camera-top={d}
+          shadow-camera-near={0.1}
+          shadow-camera-far={1500}
+          castShadow
+        />
+        <Suspense fallback={null}>
+          <FaceV1 />
+        </Suspense>
+      </Canvas>
+    </Fragment>
+  );
+};
+
+export default IndexPage;

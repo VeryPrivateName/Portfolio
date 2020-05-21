60c53da41d4333f81e034f8812157147e63f6471
Me The Admin <webcontentyo@gmail.com>
2020-05-21 17:57:51 +0200

    reactIcons

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,39 +2,66 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Style from './about.module.scss';
 
+import peep from '../images/peep.svg';
+
+import { IconContext } from 'react-icons';
+import {
+  FaChild,
+  FaFire,
+  FaCrosshairs,
+  FaSignature,
+  FaChess,
+  FaLinkedin,
+  FaGithub,
+} from 'react-icons/fa';
+
 const about = () => {
   return (
     <Layout>
       <div className={Style.grid}>
         <h1 className={Style.mtitle}>Frontend Developer</h1>
-        <div className={Style.card}>
-          <div className={Style.gridy}>
-            <i className='fas fa-child fa-lg'></i>{' '}
-            <p>
-              <span className={Style.colorspan}>Who:</span> Ignas, a Lithuanian
-              Frontend developer based in Berlin.
-            </p>
-            <i className='fas fa-fire fa-lg'></i>{' '}
-            <p>
-              <span className={Style.colorspan}>Passions:</span> all things
-              technology, music, gym, history, traveling and - very much so -
-              video games.
-            </p>
-            <i className='fas fa-crosshairs fa-lg'></i>{' '}
-            <p>
-              <span className={Style.colorspan}>Motto:</span> life is too short
-              for bad websites.
-            </p>
-            <i className='fas fa-signature fa-lg'></i>{' '}
-            <p>
-              <span className={Style.colorspan}>Languages:</span> English,
-              Lithuanian, Russian and some German.
-            </p>
-            <i className='fas fa-chess fa-lg'></i>{' '}
-            <p>
-              <span className={Style.colorspan}>Personality:</span> hungry,
-              determined, passionate, punctual, adaptive and collaborative.
-            </p>
+        <div className={Style.gridIntro}>
+          <div className={Style.graphic}>
+            <img src={peep}></img>
+          </div>
+          <div>
+            <div className={Style.card}>
+              <IconContext.Provider
+                value={{
+                  className: 'iconsProf',
+                }}
+              >
+                <div className={Style.gridy}>
+                  <FaChild />
+                  <p>
+                    <span className={Style.colorspan}>Who:</span> Ignas, a
+                    Lithuanian Frontend developer based in Berlin.
+                  </p>
+                  <FaFire />
+                  <p>
+                    <span className={Style.colorspan}>Passions:</span> all
+                    things technology, music, gym, history, traveling and - very
+                    much so - video games.
+                  </p>
+                  <FaCrosshairs />
+                  <p>
+                    <span className={Style.colorspan}>Motto:</span> life is too
+                    short for bad websites.
+                  </p>
+                  <FaSignature />
+                  <p>
+                    <span className={Style.colorspan}>Languages:</span> English,
+                    Lithuanian, Russian and some German.
+                  </p>
+                  <FaChess />
+                  <p>
+                    <span className={Style.colorspan}>Personality:</span>{' '}
+                    hungry, determined, passionate, punctual, adaptive and
+                    collaborative.
+                  </p>
+                </div>
+              </IconContext.Provider>
+            </div>
           </div>
         </div>
         <h2 className={Style.mtitle}>Skills</h2>
@@ -88,23 +115,29 @@ const about = () => {
         </div>
         <h2 className={Style.mtitle}>Contact</h2>
         <div className={Style.wrapperBorder}>
-          <div className={Style.contactholder}>
-            <a
-              href='https://www.linkedin.com/in/ignass/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <i className='fab fa-linkedin-in fa-2x'></i>{' '}
-            </a>
-            <a
-              href='https://github.com/VeryPrivateName'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <i className='fab fa-github fa-2x'></i>{' '}
-            </a>
-            <p className={Style.colorspanMail}>ignas@ignas.link</p>
-          </div>
+          <IconContext.Provider
+            value={{
+              className: 'iconsCont',
+            }}
+          >
+            <div className={Style.contactholder}>
+              <a
+                href='https://www.linkedin.com/in/ignass/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                href='https://github.com/VeryPrivateName'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaGithub />
+              </a>
+              <p className={Style.colorspanMail}>ignas@ignas.link</p>
+            </div>
+          </IconContext.Provider>
         </div>
       </div>
     </Layout>
